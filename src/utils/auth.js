6d940ba7c3aab3e4f3e09ad21e3d6e5776561fd6
200803001 <200803001@sitejiu.local>
2022-05-11 14:23:39 +0800

    拉取源代码；修复部分启动错误；

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,7 +3,8 @@ import Cookies from 'js-cookie'
 const TokenKey = 'Admin-Token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  // return Cookies.get(TokenKey)
+  return () => 'j test'
 }
 
 export function setToken(token) {
